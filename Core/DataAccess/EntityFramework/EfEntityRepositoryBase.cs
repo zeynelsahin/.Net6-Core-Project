34da96d4f67b0ff1e7383befcc4ab5afe88f7381
Zeynel Şahin <zynl.shn@hotmail.com>
2022-06-04 14:16:24 +0300

    Initial commit

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq.Expressions;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.DataAccess.EntityFramework;
+
+public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity> where TEntity : class, IEntity, new() where TContext : DbContext, new()
+{
+    public List<TEntity> GetAll(Expression<Func<TEntity, bool>>? filter = null)
+    {
+        using var context = new TContext();
+        return filter == null ? context.Set<TEntity>().ToList() : context.Set<TEntity>().Where(filter).ToList();
+    }
+
+    public TEntity Get(Expression<Func<TEntity, bool>> filter)
+    {
+        using var context = new TContext();
+        return context.Set<TEntity>().SingleOrDefault(filter);
+    }
+
+    public void Add(TEntity entity)
+    {
+        using var context = new TContext();
+        var addedEntity = context.Entry(entity);
+        addedEntity.State = EntityState.Added;
+        context.SaveChanges();
+    }
+
+    public void Update(TEntity entity)
+    {
+        using var context = new TContext();
+        var updatedEntity = context.Entry(entity);
+        updatedEntity.State = EntityState.Modified;
+        context.SaveChanges();
+    }
+
+    public void Delete(TEntity entity)
+    {
+        using var context = new TContext();
+        var deletedEntity = context.Entry(entity);
+        deletedEntity.State = EntityState.Deleted;
+        context.SaveChanges();
+    }
+
+    public async Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>>? filter = null)
+    {
+        await using var context = new TContext();
+        return filter == null ? await context.Set<TEntity>().ToListAsync() : await context.Set<TEntity>().Where(filter).ToListAsync();
+    }
+
+    public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>>? filter)
+    {
+        await using var context = new TContext();
+        return await context.Set<TEntity>().SingleOrDefaultAsync(filter);
+    }
+}
